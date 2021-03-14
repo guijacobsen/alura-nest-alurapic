b2ae0cba2a3b263fb12e3cd938d89c3ff3fec791
Guilherme Jacobsen <guijacobsen@gmail.com>
2021-03-14 14:18:17 -0300

    Tratamento de erro - Criando filtro de exceção - Ao lançar um erro, customizando objeto de erro independentemente de plataforma (express ou fastfy)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filtros/HttpExceptionFilter.filter';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [UsuarioModule],
   controllers: [],
   providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
