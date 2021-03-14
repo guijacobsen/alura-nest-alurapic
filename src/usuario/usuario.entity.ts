883faa4f2e12e4963b1c0512245fd2629f0441b6
Guilherme Jacobsen <guijacobsen@gmail.com>
2021-03-14 01:05:24 -0300

    WIP - Adicionando esquema validação na entidade Usuario

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,7 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+
 export class Usuario {
   id: number;
+
+  @IsNotEmpty({ message: 'Nome obrigatório' })
+  @IsString()
   nome: string;
+
+  @IsNotEmpty({ message: 'E-mail obrigatório' })
+  @IsEmail({}, { message: 'E-mail inválido' })
   email: string;
+
+  @IsNotEmpty({ message: 'Senha obrigatória' })
+  @Matches(/[A-Z][a-z][0-9]/)
   senha: string;
-  dataEntrada: string;
+
+  dataEntrada: Date;
 }
