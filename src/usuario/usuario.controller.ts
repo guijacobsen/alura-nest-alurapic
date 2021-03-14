7835d9effb97353df61b03d74f4a74b2909e78fc
Guilherme Jacobsen <guijacobsen@gmail.com>
2021-03-14 00:30:14 -0300

    - Tipando dados - Criando Entidade Usuario

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 
 @Controller('users')
@@ -6,12 +7,16 @@ export class UsuarioController {
   constructor(private usuarioService: UsuarioService) {}
 
   @Post('/')
-  public create(@Body() usuario) {
+  public create(@Body() usuario: Usuario): Usuario {
     return this.usuarioService.create(usuario);
   }
 
   @Get('/')
-  public get() {
-    return this.usuarioService.get();
+  public getAll() {
+    return this.usuarioService.getAll();
+  }
+  @Get(':email')
+  public getByEmail(@Param('email') email: string) {
+    return this.usuarioService.getByEmail(email);
   }
 }
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Usuario } from './usuario.entity';
 
 @Injectable()
 export class UsuarioService {
-  public usuarios = [];
+  public usuarios: Usuario[] = [];
 
-  public create(usuario) {
+  public create(usuario: Usuario): Usuario {
     this.usuarios.push(usuario);
     return usuario;
   }
-  public get() {
+  public getAll(): Usuario[] {
     return this.usuarios;
+  }
+  public getByEmail(email: string): Usuario {
+    return this.usuarios.find((usuario) => usuario.email === email);
   }
 }
